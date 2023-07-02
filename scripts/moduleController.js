1629650c90b3561923ae302230dba3983fd52ee4
Luiss510 <91172957+Luiss510@users.noreply.github.com>
2023-07-02 16:00:31 -0400

    Add files via upload

---
+++ scripts/moduleController.js
@@ -0,0 +1,39 @@
+
+document.getElementById("md1").addEventListener('click', function(){
+    localStorage.setItem('selectedModule', "mod1");
+});
+
+
+document.getElementById("md2").addEventListener('click', function(){
+    localStorage.setItem('selectedModule', "mod2");
+});
+    
+
+document.getElementById("md3").addEventListener('click', function(){
+    localStorage.setItem('selectedModule', "mod3");
+});
+
+
+document.getElementById("md4").addEventListener('click', function(){
+    localStorage.setItem('selectedModule', "mod4");
+});
+
+
+document.getElementById("md5").addEventListener('click', function(){
+    localStorage.setItem('selectedModule', "mod5");
+});
+
+
+document.getElementById("md6").addEventListener('click', function(){
+    localStorage.setItem('selectedModule', "mod6");
+});
+
+
+document.getElementById("md7").addEventListener('click', function(){
+    localStorage.setItem('selectedModule', "mod7");
+});
+
+
+document.getElementById("md8").addEventListener('click', function(){
+    localStorage.setItem('selectedModule', "mod8");
+});
